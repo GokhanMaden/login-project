b50172f0ab0a7f93dff03de4b78cc3edb5870813
Gokhan Maden <gokhanmaden1@gmail.com>
2018-07-08 16:58:22 +0300

    Authentication controls and component files created

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
+import Feed from './components/Feed'
+import Profile from './components/Profile'
+import ArticleView from './components/ArticleView'
+import Editor from './components/Editor'
+import requireAuthentication from './utils/requireAuth'
+import SignInWith from './components/SignInWith'
+
 class App extends Component {
   render() {
+    const pathName = window.location.pathname
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <div>
+        {!pathName.includes('editor') ? <Header /> : ''}
+        <SignInWith />
+        <Switch>
+          <Route path="/" component={Feed} />
+          <Route path="profile/:id" component={Profile} />
+          <Route path="articleview/:id" component={ArticleView} />
+          <Route path="/editor" component={requireAuthentication(Editor)} />
+          <Route path="**" component={Feed} />
+        </Switch>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { store, history } from './redux/store';
+import { getUser } from './redux/actions/actions';
+
+if (localStorage.Auth) {
+    // Update localStorage
+    store.dispach({
+        type: 'SET_USER',
+        user: JSON.parse(localStorage.Auth)
+    })
+
+    var _id = JSON.parse(localStorage.Auth)._id
+    getUser(_id).then((res) => {
+        store.dispach({
+            type: 'SET_USER',
+            user: res
+        })
+    })
+}
+
+ReactDOM.render((
+    <Provider store={store}>
+        <ConnectedRouter history={history} >
+            <Switch>
+                <Route path="/" component={App} />
+            </Switch>
+        </ConnectedRouter>
+    </Provider>
+), document.getElementById('root'));
 registerServiceWorker();
